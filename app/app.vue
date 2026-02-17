dc3c6b70136fdc75395403c84a6d8087c31dfdbd
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2026-02-18 01:05:33 +0800

    ➖ build: Remove vercel analytics and speed insights

--- app/app.vue
+++ app/app.vue
@@ -2,12 +2,4 @@
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
-
-  <Analytics />
-  <SpeedInsights />
 </template>
-
-<script setup lang="ts">
-import { Analytics } from "@vercel/analytics/nuxt"
-import { SpeedInsights } from "@vercel/speed-insights/nuxt"
-</script>
