3493f863ee514cdb23ca9a9707fc441c5c69b7a7
Laurence Troy Sambaan Valdez <laurencetroyv@gmail.com>
2026-02-17 21:28:11 +0800

    ⬆️ build: Update dependencies to latest versions and add zod schema validator

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import globals from "globals"
 import pluginVue from "eslint-plugin-vue"
 import perfectionist from "eslint-plugin-perfectionist"
-
 import withNuxt from "./.nuxt/eslint.config.mjs"
 
 export default withNuxt([
@@ -12,33 +11,28 @@ export default withNuxt([
       perfectionist,
     },
     rules: {
-      "perfectionist/sort-imports": [
-        "error",
-        {
-          type: "line-length",
-          order: "asc",
-          ignoreCase: true,
-          newlinesBetween: "always",
-        },
-      ],
+      "perfectionist/sort-imports": ["error"],
 
-      "perfectionist/sort-named-imports": [
-        "error",
-        {
-          type: "line-length",
-          order: "asc",
-        },
-      ],
+      "perfectionist/sort-named-imports": ["error"],
 
       "vue/no-unused-emit-declarations": ["error"],
       "vue/no-empty-component-block": ["error"],
       "vue/component-options-name-casing": ["error", "kebab-case"],
-      "vue/multi-word-component-names": ["off"],
 
       "@stylistic/quotes": ["error", "double"],
 
+      "vue/multi-word-component-names": ["off"],
+
       "semi": ["error", "never"],
       "object-curly-spacing": ["error", "always"],
+    },
+    settings: {
+      perfectionist: {
+        type: "line-length",
+        order: "asc",
+        ignoreCase: true,
+        newlinesBetween: "always",
+      },
     },
 
     languageOptions: {
